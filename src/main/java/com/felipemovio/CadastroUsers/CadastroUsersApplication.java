260c79aea3b4fadc9de468370c2a06232c9bfef5
FelipeMovio <felipelsmovio@gmail.com>
2025-10-17 18:40:25 -0300

    baixando dependencias

---
+++ src/main/java/com/felipemovio/CadastroUsers/CadastroUsersApplication.java
@@ -0,0 +1,13 @@
+package com.felipemovio.CadastroUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroUsersApplication.class, args);
+	}
+
+}
