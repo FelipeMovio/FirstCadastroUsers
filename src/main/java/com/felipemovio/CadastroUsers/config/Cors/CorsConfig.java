d39f579515886aa014e298b0095da1d89a13ba83
FelipeMovio <felipelsmovio@gmail.com>
2025-10-18 22:18:01 -0300

    instanciando a classe mapper

--- src/main/java/com/felipemovio/CadastroUsers/config/Cors/CorsConfig.java
+++ src/main/java/com/felipemovio/CadastroUsers/config/Cors/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.felipemovio.CadastroUsers.config;
+package com.felipemovio.CadastroUsers.config.Cors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/felipemovio/CadastroUsers/config/mapper/MapperConfig.java
@@ -0,0 +1,22 @@
+package com.felipemovio.CadastroUsers.config.mapper;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MapperConfig {
+//    Você criou uma classe de configuração do Spring (@Configuration)
+//    e registrou um ModelMapper como bean, permitindo que ele seja injetado automaticamente onde precisar
+
+        @Bean
+        public ModelMapper modelMapper() {
+            return new ModelMapper();
+        }
+
+//    O ModelMapper é uma biblioteca Java usada para converter automaticamente objetos de um tipo para outro, muito útil para:
+//    Converter entidades JPA em DTOs (Data Transfer Objects).
+//    Evitar get/set repetitivos.
+//    Padronizar a conversão de dados entre camadas (ex: Controller ↔ Service ↔ Repository).
+    }
+
