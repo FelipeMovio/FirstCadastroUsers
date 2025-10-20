b8684a80ff2620bd50aea14fee84001f690cd6af
FelipeMovio <felipelsmovio@gmail.com>
2025-10-20 14:13:41 -0300

    começando as regras de negocio no service

--- src/main/java/com/felipemovio/CadastroUsers/service/UserService.java
+++ src/main/java/com/felipemovio/CadastroUsers/service/UserService.java
@@ -1,10 +1,18 @@
 package com.felipemovio.CadastroUsers.service;
 
 
+import com.felipemovio.CadastroUsers.repository.UsersRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private UsersRepository usersRepository;
 
 }
