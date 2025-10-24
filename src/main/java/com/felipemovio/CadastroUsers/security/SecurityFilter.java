912694e1dad578e4f6fd8d97a1d0d26b14feab53
FelipeMovio <felipelsmovio@gmail.com>
2025-10-24 16:16:20 -0300

    mudanças no filter

--- src/main/java/com/felipemovio/CadastroUsers/security/SecurityFilter.java
+++ src/main/java/com/felipemovio/CadastroUsers/security/SecurityFilter.java
@@ -7,12 +7,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -33,8 +35,13 @@ public class SecurityFilter extends OncePerRequestFilter {
 
             if (optUser.isPresent()) {
                 JWTUserData userData = optUser.get();
+
+                List<SimpleGrantedAuthority> authorities = userData.roles().stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .toList();
+
                 UsernamePasswordAuthenticationToken authentication =
-                        new UsernamePasswordAuthenticationToken(userData, null, null);
+                        new UsernamePasswordAuthenticationToken(userData, null, authorities);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }
