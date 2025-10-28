49a65e1432ea78d0f6fc8aff3cf99f11352add04
FelipeMovio <felipelsmovio@gmail.com>
2025-10-28 18:39:47 -0300

    end point de ver todas as contas criadas feito, somente adm veem

--- src/main/java/com/felipemovio/CadastroUsers/controller/AdminController.java
+++ src/main/java/com/felipemovio/CadastroUsers/controller/AdminController.java
@@ -1,15 +1,26 @@
 package com.felipemovio.CadastroUsers.controller;
 
+import com.felipemovio.CadastroUsers.dto.response.RegisterResponseDTO;
+import com.felipemovio.CadastroUsers.model.Users;
+import com.felipemovio.CadastroUsers.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    private UsersService usersService;
+
     @GetMapping(value = "/panel")
-    public String adinPanel(){
-        return "so administradores ";
+    public ResponseEntity<List<RegisterResponseDTO>> allUsers(){
+        List<RegisterResponseDTO> users = usersService.listarTodos();
+        return ResponseEntity.ok(users);
     }
 }
