340b0ee7584249791acc99d36f4b539aa1022737
FelipeMovio <felipelsmovio@gmail.com>
2025-10-30 19:05:55 -0300

    end point de deletar user

--- src/main/java/com/felipemovio/CadastroUsers/controller/UserController.java
+++ src/main/java/com/felipemovio/CadastroUsers/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.felipemovio.CadastroUsers.controller;
 
-import com.felipemovio.CadastroUsers.dto.request.RegisterRequestDTO;
 import com.felipemovio.CadastroUsers.dto.request.UpdateUserRequestDTO;
 import com.felipemovio.CadastroUsers.dto.response.RegisterResponseDTO;
-import com.felipemovio.CadastroUsers.security.JWTUserData;
 import com.felipemovio.CadastroUsers.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,5 +24,11 @@ public class UserController {
             @RequestBody UpdateUserRequestDTO dto
     ) {
         return ResponseEntity.ok(userService.atualizarUsuarioLogado( dto.getNome(), dto.getIdade(), dto.getNovaSenha()));
+    }
+
+    @DeleteMapping("/me")
+    public void deletarUser(){
+        userService.deletarUsuarioLogado();
+
     }
 }
