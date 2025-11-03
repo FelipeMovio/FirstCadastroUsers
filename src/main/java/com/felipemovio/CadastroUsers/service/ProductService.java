57b72dce31e9b581f891142ad4197a5757f526ca
FelipeMovio <felipelsmovio@gmail.com>
2025-11-03 20:03:04 -0300

    create service product

---
+++ src/main/java/com/felipemovio/CadastroUsers/service/ProductService.java
@@ -0,0 +1,23 @@
+package com.felipemovio.CadastroUsers.service;
+
+import com.felipemovio.CadastroUsers.model.Product;
+import com.felipemovio.CadastroUsers.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    // ver itens listas
+    public List<Product> verProdutos(){
+        return repository.findAll();
+
+    }
+}
