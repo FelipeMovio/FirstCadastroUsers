1f523d20e8596492cdaa6a6595c933c329dc53fa
FelipeMovio <felipelsmovio@gmail.com>
2025-11-04 19:22:15 -0300

    update

--- src/main/java/com/felipemovio/CadastroUsers/controller/ProductController.java
+++ src/main/java/com/felipemovio/CadastroUsers/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.felipemovio.CadastroUsers.model.Product;
 import com.felipemovio.CadastroUsers.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,8 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+
+    @GetMapping
     public ResponseEntity<List<Product>> verTodos(){
         List<Product> product = productService.verProdutos();
         return ResponseEntity.ok(product);
