832f51b5f0a1bd545ac93d6aa3a62c2d48ba43cc
FelipeMovio <felipelsmovio@gmail.com>
2025-11-04 19:08:29 -0300

    controller de ver todos produtos criado

--- src/main/java/com/felipemovio/CadastroUsers/controller/ProductController.java
+++ src/main/java/com/felipemovio/CadastroUsers/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.felipemovio.CadastroUsers.controller;
 
+import com.felipemovio.CadastroUsers.model.Product;
 import com.felipemovio.CadastroUsers.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -11,4 +15,9 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    public ResponseEntity<List<Product>> verTodos(){
+        List<Product> product = productService.verProdutos();
+        return ResponseEntity.ok(product);
+    }
 }
