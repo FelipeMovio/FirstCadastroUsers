5cd0900ae79df5c859b62de8a8417d5d97cbaa2c
FelipeMovio <felipelsmovio@gmail.com>
2025-11-04 19:04:46 -0300

    criando end point dos produtos

--- src/main/java/com/felipemovio/CadastroUsers/controller/ProductController.java
+++ src/main/java/com/felipemovio/CadastroUsers/controller/ProductController.java
@@ -1,4 +1,14 @@
 package com.felipemovio.CadastroUsers.controller;
 
+import com.felipemovio.CadastroUsers.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
 }
